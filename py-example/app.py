4b3fe9185aaac1815608ebceffbf6778eac62a4d
Omeed Musavi <omusavi@microsoft.com>
2018-12-04 20:37:23 -0800

    Add extra_data value to hello world

--- py-example/app.py
+++ py-example/app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World! Build Number: " + os.getenv('extra_data', "none")
 
 if __name__ == '__main__':
     app.run()
